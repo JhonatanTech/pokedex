ea8ecd366f9ab23fcf2fa389cb41d56c77248424
Jhonatan Carvalho <jhonatancarvalhotorres12@gmail.com>
2022-07-02 03:59:36 -0300

    'fix'

--- js/vendor/request.js
+++ js/vendor/request.js
@@ -1,6 +1,6 @@
 // https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0
 
-fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+fetch('https://pokeapi.co/api/v2/pokemon?limit=11&offset=0')
     .then(response=>response.json())
     .then(results => {
         const pokemons = results.results
@@ -15,17 +15,40 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
                         img: result.sprites.other.home.front_default,
                         imgShiny: result.sprites.other.home.front_shiny,
                         type: result.types[0].type.name,
+                        types: result.types,
+                        hp: result.stats[0].base_stat,
+                        atk: result.stats[1].base_stat,
+                        df: result.stats[2].base_stat,
+                        spd: result.stats[5].base_stat
                     }
+
+                    console.log(pokemonFeatures);
+
                     const container = document.querySelector('.pokemons')
                     card = `<div class="pokemons_div_card ${pokemonFeatures.type}">
                                 <img class="pokemons_img_pokemon img-${pokemonFeatures.type}" src="${pokemonFeatures.img}" alt="${pokemonFeatures.name}">
                                 <p class="pokemons_p_number">#${pokemonFeatures.id.toString().padStart(4,'0')}</p>
                                 <p class="pokemons_p_name">${pokemonFeatures.name}</p>
-                                <div class="flags">
-                                    <span class="pokemons_span_flag ${pokemonFeatures.type}">${pokemonFeatures.type}</span>
-                                </div>
-                            </div>`
-                    container.innerHTML += card
+                                <div class="flags">`
+                    
+                    pokemonFeatures.types.forEach(type => {
+                        type = `<span class="pokemons_span_flag ${type.type.name}">${type.type.name}</span>`
+                        card+=type
+                    })
+                    
+                    container.innerHTML += card + `
+                    </div>
+                        <div class="stats ${pokemonFeatures.type}">
+                            <p class="stats_p_stat">Hp</p>
+                            <p class="stats_p_value">${pokemonFeatures.hp}</p>
+                            <p class="stats_p_stat">attack</p>
+                            <p class="stats_p_value">${pokemonFeatures.atk}</p>
+                            <p class="stats_p_stat">defense</p>
+                            <p class="stats_p_value">${pokemonFeatures.df}</p>
+                            <p class="stats_p_stat">speed</p>
+                            <p class="stats_p_value">${pokemonFeatures.spd}</p>
+                        </div>
+                    </div>`
                 })
         })
 })

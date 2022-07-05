08e3d69a9a8b19084b449373fa8205d5f4fe22fd
Jhonatan Carvalho <jhonatancarvalhotorres12@gmail.com>
2022-07-05 01:41:39 -0300

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer')
+const concat = require('gulp-concat')
+var csso = require('gulp-csso');
 
 function buildStyles() {
     return gulp.src('sass/vendor/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(csso())
+        .pipe(concat('all.css'))
         .pipe(gulp.dest('sass/'))
 }
 exports.buildStyles = buildStyles
